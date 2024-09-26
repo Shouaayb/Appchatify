54088cc488b9d9a4eab0ca1259bb95d3b5e92a42
Shouaayb <shouaaybjiddy@yahoo.com>
2024-09-26 10:50:46 +0200

    slut

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-router-dom', 'bootstrap/dist/css/bootstrap.min.css'],
+      external: ['react-router-dom'], // Låt React Router vara extern
       output: {
         globals: {
-          'react-router-dom': 'ReactRouterDOM',
-          'bootstrap/dist/css/bootstrap.min.css': 'BootstrapCSS'
+          'react-router-dom': 'ReactRouterDOM'
         }
       }
     }
   }
-})
+});
